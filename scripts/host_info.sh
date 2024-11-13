12eef70ca2d63d3f60c4c66d2f36eb693f23a576
Piyush Sethi <piyushs8e@gmail.com>
2024-11-13 07:41:37 +0000

    host info and usage scripts

--- scripts/host_info.sh
+++ scripts/host_info.sh
@@ -21,6 +21,15 @@ l2_cache=$(echo "$lscpu_out" | egrep "^L2 cache:" | awk '{print $3}' | xargs)
 total_mem=$(vmstat --unit M | tail -1 | awk '{print $4}' | xargs)
 timestamp=$(date "+%Y-%m-%d %H:%M:%S")
 
+echo "Hostname: $hostname"
+echo "CPU Number: $cpu_number"
+echo "CPU Architecture: $cpu_architecture"
+echo "CPU Model: $cpu_model"
+echo "CPU MHz: $cpu_mhz"
+echo "L2 Cache: $l2_cache KB"
+echo "Total Memory: $total_mem MB"
+echo "Timestamp: $timestamp"
+
 insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp) VALUES ('$hostname', $cpu_number, '$cpu_architecture', '$cpu_model', $cpu_mhz, $l2_cache, $total_mem, '$timestamp');"
 
 export PGPASSWORD=$psql_password
